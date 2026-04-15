3ea909410b03a14ecebdf6ea9ce5c004692b9294
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-04-15 21:14:20 +0500

    waqas  partice codde
    waqas  partice codde

---
+++ Assigment code/PR.cpp
@@ -0,0 +1,37 @@
+
+#include<iostream>
+using namespace std ;
+int main (){
+	
+	int 
+	
+	   //////var -----
+	   ///    variable is used to store  dta
+	    //ruule to make varivake namee
+	    
+	    ///////vartiablr   name does   not start  with   couting (1-9 )
+	    
+//	    int 123 = 12;     wrong way
+
+//        int   a   b =12;   //./   not space betwwn variasble name 
+//            int   a-b =12 ;    /// not exist underscore
+//     int a_b   = 12 ;	    
+
+
+//             ------------data type = 5
+             //   1 - char   it  must conatian conatin sinbgme cxoma ,   int '2';
+             //  2  - boolen  --------  0 -=   false  ---  true  ----1
+            //    3-  string       ===""    
+            //   5 -  float 
+            
+               int rrt =123 ;
+              	char a = 'a';
+              	float point  = 1.3 ;
+              	bool check  = 0;
+              	string shah = "ali";
+              	cout<<rrt;
+              	
+              	
+	
+return 0;
+}
