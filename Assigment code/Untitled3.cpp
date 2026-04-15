fbc17951c256ddfa890330e1efaa85237d31fbf2
Muhammad Junaid Hassan <chjunaidhassan95@gmail.com>
2026-04-15 21:15:07 +0500

    waqas paratiuce
    waqas paratiuce

---
+++ Assigment code/Untitled3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	
+	
+string boss = "juni";	
+string musa = "juni2";
+string kusa = "waqas";
+string fusa = "waqas2";
+	
+	cout<<fusa<<endl<<boss<<endl<<musa<<endl<<kusa<<endl;
+	
+	
+	
+	return 0;
+}
